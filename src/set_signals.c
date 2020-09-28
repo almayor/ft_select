cf0c1546f18b523b53037413a92f8c0ca95cf0b0
almayor <mayorovme@gmail.com>
2020-09-29 00:12:53 +0300

    random string message change

--- src/set_signals.c
+++ src/set_signals.c
@@ -6,7 +6,7 @@
 /*   By: unite <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 14:04:53 by unite             #+#    #+#             */
-/*   Updated: 2020/09/28 23:19:33 by unite            ###   ########.fr       */
+/*   Updated: 2020/09/29 00:12:44 by unite            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static void		signal_handler(int sig)
 {
 	if (sig == SIGSEGV)
-		fatal("You found a segfault! Good for you! 🤩💪");
+		fatal("You found a segfault! 💪 Good for you! 🤩");
 	else if (sig == SIGTSTP)
 	{
 		set_terminal(RESET);
